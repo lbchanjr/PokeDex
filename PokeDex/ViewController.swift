d0cc951c4f1b5c3ff8ae05817b152bd718900f7c
Louise Chan <lbchan525@gmail.com>
2020-05-25 12:19:41 -0400

    Initial Commit

---
+++ PokeDex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PokeDex
+//
+//  Created by Louise Chan on 2020-05-25.
+//  Copyright © 2020 Louise Chan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
